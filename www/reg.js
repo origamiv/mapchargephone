6d2eba19cfa80aeadbe15055dc092a773b8e58e4
Veniamin Smorodinsky <origamiv@mail.ru>
2017-09-13 23:23:34 +0300

    регистрация

--- www/reg.js
+++ www/reg.js
@@ -2,9 +2,18 @@ $( document ).ready(function()
         {
            
                 $("#breg").bind("click", function(){
-                    //var z=$("#page1").css('left');
+                    var fio=$("#fio").val();
+                    var login=$("#login").val();
+                    var pass=$("#pass").val();
+                    var vozrast=$("#vozrast").val();
                     
-                    alert('mmm');
+                    var url='http://integralkin.ru/projects/mapchargephone/mobile_reg.php?fio='+fio+'&login='+login+'&pass='+pass+'&vozrast='+vozrast;
+                    $.get( url, function( data ) {
+                            //$( ".result" ).html( data );
+                        alert( "Вы зарегистрированы");
+                        document.location='second.html';
+                    });
+                    //alert('mmm');
                     
                 });               
                 
